004057daca3411a1b15b59dd9aca2098ef402c37
MatiasHauria <hauriatute@gmail.com>
2025-09-05 15:28:02 -0300

    "Se agrega las bases del ejercicio 1"

---
+++ src/Ejercicio1/Productos.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio1;
+
+/**
+ *
+ * @author matute
+ */
+public class Productos {
+    private String nombre;
+    private int precio;
+
+    public Productos(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre: " + nombre;
+    }
+    
+    
+}
